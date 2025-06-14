3ac86f73eb7ad639bc8628a1e232350a11c3098b
Aphid <u26480@gmail.com>
2025-06-14 17:39:29 +0800

    fix: 抑制 react-beautiful-dnd 的 defaultProps 警告

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,6 +3,15 @@ import React from 'react';
 import App from './App.tsx';
 import './index.css';
 
+// 暫時抑制 react-beautiful-dnd 的 defaultProps 警告
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (args[0] && args[0].includes('Support for defaultProps will be removed from memo components')) {
+    return;
+  }
+  originalWarn.apply(console, args);
+};
+
 const container = document.getElementById('root');
 if (!container) throw new Error('Root element not found');
 
