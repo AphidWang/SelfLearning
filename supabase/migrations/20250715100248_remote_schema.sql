08ac5c1a73381b74b1fd604eb6d4c75e4b1d277a
Aphid <u26480@gmail.com>
2025-07-16 16:25:01 +0800

    gitignore

--- supabase/migrations/20250715100248_remote_schema.sql
+++
@@ -1,7 +0,0 @@
-alter table "public"."daily_journals" drop constraint "daily_journals_mood_check";
-
-alter table "public"."daily_journals" add constraint "daily_journals_mood_check" CHECK (((mood)::text = ANY ((ARRAY['excited'::character varying, 'happy'::character varying, 'okay'::character varying, 'tired'::character varying, 'stressed'::character varying])::text[]))) not valid;
-
-alter table "public"."daily_journals" validate constraint "daily_journals_mood_check";
-
-
